059f97f3f0ead4f10296257509e8e86e236cbc9b
efftushkin <efftushkin@ukr.net>
2021-05-26 21:47:17 +0300

    OperaDriver doesn't support headless mode

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -9,8 +9,9 @@ public class Driver {
 
     public static WebDriver getWebDriver() {
         if (webDriver == null) {
-            System.setProperty("webdriver.opera.driver", "src/test/resources/operadriver_win32/operadriver.exe");
+            System.setProperty("webdriver.opera.driver", "src/test/resources/operadriver_win64/operadriver.exe");
             webDriver = new OperaDriver();
+            //OperaDriver doesn't support headless mode 0_o
         }
 
         return webDriver;
